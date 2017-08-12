f96c42e1a82a63b50af8893a4c5d3acb7c4cd838
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-08-12 20:37:18 +0800

    Added Scraper class.

--- RawDataWorker.py
+++ RawDataWorker.py
@@ -1,11 +1,18 @@
 from Repository import Repository
-from TripAdvisorProfile import extract_profile
+from Scraper import Scraper
+import requests
+from bs4 import BeautifulSoup
 
 class RawDataWorker:
 
-    def write_raw_users(self, soups):
+    def write_raw_users(self, urls):
         data = []
-        for soup in soups:
-            data.append(extract_profile(soup))
+        scraper = Scraper()
+
+        for url in urls:
+            soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+            data.append(scraper.extract_user(soup))
+
         repo = Repository()
         repo.write_raw_users(data)
+
