e80918957974e43ef2b96ed7c8904e343c3ea8f7
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-08-13 08:13:42 +0800

    Created a console interface.

--- Main.py
+++ Main.py
@@ -1,7 +1,23 @@
 from workers.RawDataWorker import RawDataWorker
 
 raw_data_worker = RawDataWorker()
-# raw_data_worker.extract_raw_reviews_singapore_zoo()
-# raw_data_worker.extract_raw_reviews_river_safari()
-# raw_data_worker.extract_raw_reviews_night_safari()
-raw_data_worker.extract_raw_users_all_attractions()
+actions = {
+    "1": {"name": "Get raw reviews from Singapore Zoo", "execute": "raw_data_worker.extract_raw_reviews_singapore_zoo()"},
+    "2": {"name": "Get raw reviews from River Safari", "execute": "raw_data_worker.extract_raw_reviews_river_safari()"},
+    "3": {"name": "Get raw reviews from Night Safari", "execute": "raw_data_worker.extract_raw_reviews_night_safari()"},
+    "4": {"name": "Get raw user profiles from all attractions", "execute": "raw_data_worker.extract_raw_users_all_attractions()"}
+}
+
+print("####################################")
+print("## WildLife Reserves Web Scraping ##")
+print("####################################")
+for action_key, action_item in actions.items():
+    print(action_key + ". " + action_item['name'])
+
+choice = input("Please enter your choice.")
+
+if choice not in actions:
+    print("'" + choice + "' is invalid choice.")
+else:
+    eval(actions[choice]['execute'])
+
