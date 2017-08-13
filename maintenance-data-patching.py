fae6f8757c5c0ebf68c0bd14ae608b18279cccb3
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-08-13 22:43:59 +0800

    Updated maintenance scripts.

--- maintenance-data-patching.py
+++ maintenance-data-patching.py
@@ -12,11 +12,19 @@ class Patch():
         repo = Repository()
         url = "https://www.tripadvisor.com.sg/Attraction_Review-g294265-d324542-Reviews-Singapore_Zoo-Singapore.html"
 
-        repo.write_raw_reviews_singapore_zoo_families(scraper.extract_reviews("Singapore Zoo", url, "Families"))
+        # repo.write_raw_reviews_singapore_zoo_families(scraper.extract_reviews("Singapore Zoo", url, "Families"))
         # repo.write_raw_reviews_singapore_zoo_couples(scraper.extract_reviews("Singapore Zoo", url, "Couples"))
         # repo.write_raw_reviews_singapore_zoo_solo(scraper.extract_reviews("Singapore Zoo", url, "Solo")) # Done
         # repo.write_raw_reviews_singapore_zoo_business(scraper.extract_reviews("Singapore Zoo", url, "Business")) # Done
         # repo.write_raw_reviews_singapore_zoo_friends(scraper.extract_reviews("Singapore Zoo", url, "Friends"))
+
+        # data = []
+        # data += repo.read_raw_reviews_singapore_zoo_families()
+        # data += repo.read_raw_reviews_singapore_zoo_couples()
+        # data += repo.read_raw_reviews_singapore_zoo_solo()
+        # data += repo.read_raw_reviews_singapore_zoo_business()
+        # data += repo.read_raw_reviews_singapore_zoo_friends()
+        # repo.write_raw_reviews_all_attractions(data)
 
 Patch().run_patch()
 
--- maintenance-json-backup.py
+++ maintenance-json-backup.py
@@ -4,10 +4,12 @@ import json
 
 repo = Repository()
 # json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_families()), open("raw_data_backups/raw_reviews_singapore_zoo_families.json", "w"))
-json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_couples()), open("raw_data_backups/raw_reviews_singapore_zoo_couples.json", "w"))
+# json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_couples()), open("raw_data_backups/raw_reviews_singapore_zoo_couples.json", "w"))
 # json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_solo()), open("raw_data_backups/raw_reviews_singapore_zoo_solo.json", "w"))
 # json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_business()), open("raw_data_backups/raw_reviews_singapore_zoo_business.json", "w"))
 # json.dump(json_util.dumps(repo.read_raw_reviews_singapore_zoo_friends()), open("raw_data_backups/raw_reviews_singapore_zoo_friends.json", "w"))
 
 # json.dump(json_util.dumps(repo.read_raw_reviews_river_safari()), open("raw_data_backups/raw_reviews_river_safari.json", "w"))
 # json.dump(json_util.dumps(repo.read_raw_reviews_night_safari()), open("raw_data_backups/raw_reviews_night_safari.json", "w"))
+
+json.dump(json_util.dumps(repo.read_raw_reviews()), open("raw_data_backups/raw_reviews.json", "w"))
