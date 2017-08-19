f6b54fab2c8680702df54cf9b92e5685bd88bb0c
HuaqingWu <neilwu0603@gmail.com>
2017-08-19 10:18:10 +0800

    Added mac chromedriver and data preprocessing scripts.

--- workers/ProcessDataWorker.py
+++ workers/ProcessDataWorker.py
@@ -21,6 +21,22 @@ class ProcessDataWorker:
         reviews = self.repo.read_raw_reviews()
         for review in reviews:
             review['uid'] = regex.compile('UID_(.*)-SRC').search(review['uid']).group(1) if review['uid'] != '' else ''
+            review['review_header'] = review['review_header'].replace("\n", " ").replace(",", "").replace("  ", " ")
+            review['review_body'] = review['review_body'].replace("\n", " ").replace(",", "").replace("  ", " ")
+            if review['user_name'] == "A TripAdvisor Member":
+                review['username'] = "0"
+            else:
+                review['username'] = review['user_name']
+            review.pop('user_name')
+            month = regex.compile('\d+\s+(\S+)\s+\d+').search(review['review_date']).group(1)
+            if month in ['January','February','March']:
+                review['review_quarter'] = "1"
+            elif month in ['April','May','June']:
+                review['review_quarter'] = "2"
+            elif month in ['July','August','September']:
+                review['review_quarter'] = "3"
+            else:
+                review['review_quarter'] = "4"
         print("Data processing is done for raw reviews, data is then copied to collection 'processed_reviews'.")
         self.repo.write_processed_reviews(reviews)
 
@@ -52,6 +68,73 @@ class ProcessDataWorker:
                 gender = ''
             user['gender'] = gender
             user.pop('short_desc')
+
+            # Get 'country' from 'hometown'
+            # Remove 'hometown' 
+            countries = ['afghanistan', 'albania', 'algeria', 'andorra', 'angola', 'antigua and barbuda', 'argentina',
+                         'armenia', 'aruba', 'australia', 'austria', 'azerbaijan', 'bahamas, the', 'bahrain',
+                         'bangladesh', 'barbados', 'belarus', 'belgium', 'belize', 'benin', 'bhutan', 'bolivia',
+                         'bosnia and herzegovina', 'botswana', 'brazil', 'brunei', 'bulgaria', 'burkina faso', 'burma',
+                         'burundi', 'cabo verde', 'cambodia', 'cameroon', 'canada', 'central african republic', 'chad',
+                         'chile', 'china', 'colombia', 'comoros', 'congo, democratic republic of the congo',
+                         'republic of the costa rica', 'croatia', 'cuba', 'curacao', 'cyprus', 'czechia', 'denmark',
+                         'djibouti', 'dominica', 'dominican republic', 'east timor (see timor-leste)', 'ecuador',
+                         'egypt', 'el salvador', 'equatorial guinea', 'eritrea', 'estonia', 'ethiopia', 'fiji',
+                         'finland', 'france', 'gabon', 'gambia, the', 'georgia', 'germany', 'ghana', 'greece',
+                         'grenada', 'guatemala', 'guinea', 'guinea-bissau', 'guyana', 'haiti', 'holy see', 'honduras',
+                         'hungary', 'iceland', 'india', 'indonesia', 'iran', 'iraq', 'ireland', 'israel', 'italy',
+                         'jamaica', 'japan', 'jordan', 'kazakhstan', 'kenya', 'kiribati', 'korea, north',
+                         'korea, south', 'kosovo', 'kuwait', 'kyrgyzstan', 'laos', 'latvia', 'lebanon', 'lesotho',
+                         'liberia', 'libya', 'liechtenstein', 'lithuania', 'luxembourg', 'macedonia', 'madagascar',
+                         'malawi', 'malaysia', 'maldives', 'mali', 'malta', 'marshall islands', 'mauritania',
+                         'mauritius', 'mexico', 'micronesia', 'moldova', 'monaco', 'mongolia', 'montenegro', 'morocco',
+                         'mozambique', 'namibia', 'nauru', 'nepal', 'netherlands', 'new zealand', 'nicaragua', 'niger',
+                         'nigeria', 'north korea', 'norway', 'oman', 'pakistan', 'palau', 'palestinian territories',
+                         'panama', 'papua new guinea', 'paraguay', 'peru', 'philippines', 'poland', 'portugal', 'qatar',
+                         'romania', 'russia', 'rwanda', 'saint kitts and nevis', 'saint lucia',
+                         'saint vincent and the grenadines', 'samoa', 'san marino', 'sao tome and principe',
+                         'saudi arabia', 'senegal', 'serbia', 'seychelles', 'sierra leone', 'singapore', 'sint maarten',
+                         'slovakia', 'slovenia', 'solomon islands', 'somalia', 'south africa', 'south korea',
+                         'south sudan', 'spain', 'sri lanka', 'sudan', 'suriname', 'swaziland', 'sweden', 'switzerland',
+                         'syria', 'tajikistan', 'tanzania', 'thailand', 'timor-leste', 'togo', 'tonga',
+                         'trinidad and tobago', 'tunisia', 'turkey', 'turkmenistan', 'tuvalu', 'uganda', 'ukraine',
+                         'united arab emirates', 'united kingdom', 'uruguay', 'uzbekistan', 'vanuatu', 'venezuela',
+                         'vietnam', 'yemen', 'zambia', 'zimbabwe']
+            if regex.search("\,", user['hometown']):
+                user_country = regex.compile('(.*)\,(.*)').search(user['hometown']).group(2).strip().lower()
+            else:
+                user_country = user['hometown'].strip().lower()
+            user['country'] = user_country if user_country in countries else "nil"
+            user.pop('hometown')
+
+            # travel style transformation
+            ts_list = ['travel_style_60plus_traveller', 'travel_style_art_architecture_lover', 'travel_style_backpacker',
+             'travel_style_beach_goer', 'travel_style_eco_tourist', 'travel_style_family_holiday_maker',
+             'travel_style_foodie', 'travel_style_history_buff', 'travel_style_like_a_local',
+             'travel_style_luxury_traveller', 'travel_style_nature_lover', 'travel_style_nightlife_seeker',
+             'travel_style_peace_quiet_seeker', 'travel_style_shopping_fanatic', 'travel_style_thrifty_traveller',
+             'travel_style_thrill_seeker', 'travel_style_trendsetter', 'travel_style_urban_explorer',
+             'travel_style_vegetarian']
+            for ts in ts_list:
+                user[ts] = 0
+            travel_styles = ['60+ Traveller', 'Art and Architecture Lover', 'Backpacker', 'Beach Goer', 'Eco-tourist',
+                             'Family Holiday Maker', 'Foodie', 'History Buff', 'Like a Local', 'Luxury Traveller',
+                             'Nature Lover', 'Nightlife Seeker', 'Peace and Quiet Seeker', 'Shopping Fanatic',
+                             'Thrifty Traveller', 'Thrill Seeker', 'Trendsetter', 'Urban Explorer', 'Vegetarian']
+            if user['travel_style'] != []:
+                for ts in user['travel_style']:
+                    if ts in travel_styles:
+                        p = travel_styles.index(ts)
+                        user[ts_list[p]] = 1
+            user.pop('travel_style')
+            #
+            if regex.search('\d+',user['visited_cities']):
+                user['visited_cities'] = regex.compile('(\d+)\s+.*').search(user['visited_cities']).group(1)
+            if regex.search('\d+',user['helpful_votes']):
+                user['helpful_votes'] = regex.compile('(\d+)\s+.*').search(user['helpful_votes']).group(1)
+            if regex.search('\d+',user['passport_badge']):
+                user['passport_badge'] = regex.compile('(\d+)\s+.*').search(user['passport_badge']).group(1)
+            #
 
         print("Data processing is done for raw users, data is then copied to collection 'processed_users'.")
         self.repo.write_processed_users(users)
